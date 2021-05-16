a501fc13469357bd58dfe857a200d45a9d6f3700
kkan0615 <kkan0615@gmail.com>
2021-05-16 22:11:59 +0900

    :rocket: first commit

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
+import '@/styles/index.scss'
 import router from './router'
-import store from './store'
+import { store, key } from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App)
+
+app
+  .use(store, key)
+  .use(router)
+  .mount('#app')
